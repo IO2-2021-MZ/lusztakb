4ed5e7f934af814af0069726dcb1ddb2ef856196
b.lusztak <b.lusztak@student.mini.pw.edu.pl>
2021-03-09 17:29:09 +0100

    Initial project addition

---
+++ StringCalclator/StringCalclator/Class1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        public int Add(String input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return 0;
+
+            List<String> sepList = new List<String>();
+            sepList.Add(",");
+            sepList.Add("\n");
+
+            if (input.Length > 2)
+            {
+                if (input[0] == '/' && input[1] == '/' && input[3] == '\n')
+                {
+                    sepList.Add(input[2].ToString());
+                    input = input.Substring(4, input.Length - 4);
+                }
+                else
+                {
+                    if (input.Length > 4)
+                    {
+                        if (input[0] == '/' && input[1] == '/' && input[2] == '[')
+                        {
+
+                            int i = 3;
+                            while (input[i] != '\n')
+                            {
+                                string multiCharDelimeter = "";
+                                while (input[i] != ']')
+                                {
+                                    multiCharDelimeter += input[i];
+                                    i++;
+                                }
+                                sepList.Add(multiCharDelimeter);
+                                i++;
+                                if (input[i] == '[')
+                                {
+                                    i++;
+                                }
+                            }
+                            input = input.Substring(1 + i, input.Length - (1 + i));
+                        }
+                    }
+                }
+            }
+
+            string[] numbers = input.Split(sepList.ToArray(), StringSplitOptions.None);
+            int sum = 0;
+            foreach (string number in numbers)
+            {
+                int num = int.Parse(number);
+                if (num < 0)
+                {
+                    throw new Exception("Negative number");
+                }
+
+                if (num > 1000)
+                {
+                    continue;
+                }
+
+                sum += num;
+            }
+            return sum;
+        }
+
+    }
+}
---
+++ StringCalclator/Tests/UnitTest1.cs
@@ -0,0 +1,154 @@
+using NUnit.Framework;
+using System;
+
+namespace StringCalculator.Test
+{
+    public class CalculatorTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void EmptyStringReturnZero()
+        {
+            Calculator calculator = new Calculator();
+            string input = "";
+            int expectedOutput = 0;
+            int output = calculator.Add(input);
+
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [Test]
+        public void SingleNumberReturnsValue()
+        {
+            Calculator calculator = new Calculator();
+            string input = "7";
+            int expectedOutput = 7;
+            int output = calculator.Add(input);
+
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [Test]
+        public void TwoNumbersCommaDelimitedReturnsTheSum()
+        {
+            Calculator calculator = new Calculator();
+            string input = "7,23";
+            int expectedOutput = 30;
+            int output = calculator.Add(input);
+
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [Test]
+        public void TwoNumbersNewlineDelimitedReturnsTheSum()
+        {
+            Calculator calculator = new Calculator();
+            string input = "7\n33";
+            int expectedOutput = 40;
+            int output = calculator.Add(input);
+
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [Test]
+        public void ThreeNumbersCommaDelimitedReturnsTheSum()
+        {
+            Calculator calculator = new Calculator();
+            string input = "7,23,30";
+            string input2 = "7,23,30";
+            int expectedOutput = 60;
+            int output = calculator.Add(input);
+
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [Test]
+        public void ThreeNumbersNewlineDelimitedReturnsTheSum()
+        {
+            Calculator calculator = new Calculator();
+            string input = "7\n33\n10";
+            int expectedOutput = 50;
+            int output = calculator.Add(input);
+
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [Test]
+        public void ThreeNumbersNewlineCommaDelimitedReturnsTheSum()
+        {
+            Calculator calculator = new Calculator();
+            string input = "7\n33,17,3\n10";
+            int expectedOutput = 70;
+            int output = calculator.Add(input);
+
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [Test]
+        public void NegativeNumbersThrowAnException()
+        {
+            Calculator calculator = new Calculator();
+            string input = "-20,20\n-44";
+            try
+            {
+                int output = calculator.Add(input);
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+
+                Assert.AreEqual(e.Message, "Negative number");
+            }
+        }
+
+        [Test]
+        public void NumbersGreaterThan1000AreIgnored()
+        {
+            Calculator calculator = new Calculator();
+            string input = "5,10000\n30";
+            int expectedOutput = 35;
+            int output = calculator.Add(input);
+
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [Test]
+        public void SingleCharDelimeterCanBeDefinedOnTheFirstLine()
+        {
+            Calculator calculator = new Calculator();
+            string input = "//#\n5,100#100\n30";
+            int expectedOutput = 235;
+            int output = calculator.Add(input);
+
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [Test]
+        public void MultiCharDelimeterCanBeDefinedOnTheFirstLine()
+        {
+            Calculator calculator = new Calculator();
+            string input = "//[bbb]\n5,100bbb100\n30";
+            int expectedOutput = 235;
+            int output = calculator.Add(input);
+
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [Test]
+        public void ManySingleOrMultiCharDelimitersCanBeDefined()
+        {
+            Calculator calculator = new Calculator();
+            string input = "//[bbb][aaa]\n5,100bbb100aaa215\n30";
+            int expectedOutput = 450;
+            int output = calculator.Add(input);
+
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+
+    }
+}
